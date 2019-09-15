c5fd986ec38a52af12b278d03c3cb0848bf44ac0
Tharun <tharun@Tharuns-MacBook-Pro.local>
2019-09-16 01:04:53 +0530

    Modified SystemParameters

---
+++ Services/SystemParameters/bin/canister.js
@@ -0,0 +1,20 @@
+var canister = {
+    "metadata": {
+      "timeStamp": "value",
+      "payLoadType": "value",
+      "service": "value",
+      "subService": "value",
+      "sessionToken":"value"
+    },
+    "payLoad": [
+      {
+        
+      }
+    ],
+    "payLoadType":"value",
+    "error":{
+        "code":"",
+        "description":""
+    }
+  }
+module.exports = canister
